cd80539e6d959fc54ec04ce72d2deed082266bb6
Wataru Okamoto <wokamoto.talend.com@talendconsultingap.aws>
2021-06-23 04:04:22 +0900

    User 'wokamoto.talend.com@talendconsultingap.aws' at 04:04:20 JST 2021-06-23 : User action : ジョブの編集 : Test_Job01

---
+++ WOKAMOTO_PROJECT/poms/jobs/process/test_job01_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-to-slf4j-2.12.1.jar:$ROOT_PATH/../lib/slf4j-log4j12-1.7.25.jar:$ROOT_PATH/../lib/log4j-1.2.17.jar:$ROOT_PATH/../lib/commons-lang3-3.8.1.jar:$ROOT_PATH/../lib/accessors-smart-1.1.jar:$ROOT_PATH/../lib/audit-common-1.8.0.jar:$ROOT_PATH/../lib/slf4j-api-1.7.25.jar:$ROOT_PATH/../lib/logging-event-layout-1.8.0.jar:$ROOT_PATH/../lib/asm-5.0.3.jar:$ROOT_PATH/../lib/job-audit.jar:$ROOT_PATH/../lib/json-smart-2.2.1.jar:$ROOT_PATH/../lib/audit-log4j1-1.8.0.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/test_job01_0_1.jar: wokamoto_project.test_job01_0_1.Test_Job01  "$@"
